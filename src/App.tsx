4ecf1fad878b0526d30948282c4e2013a20a2dbc
JT Broad <jbroad@mainsequence.net>
2021-01-29 15:12:33 -0500

    intro components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { css } from '@emotion/css';
 
-function App() {
+interface SecondButtonProps {
+  otherButtonCount:number;
+}
+
+export class SecondButton extends React.Component<SecondButtonProps, any>{
+
+  state = {
+    clickCount:0,
+  }
+
+  componentDidMount(){
+
+  }
+
+  componentDidUpdate(){
+
+  }
+
+  componentWillUnmount(){
+
+  }
+
+  clickedButton = ()=>{
+    this.setState({
+      clickCount:(this.state.clickCount + 1)
+    })
+  }
+
+  render(){
+    return <button 
+      className={css`
+        background-color:yellow;
+      `}
+      onClick={this.clickedButton}>
+      Some text {this.state.clickCount} || otherbutton: {this.props.otherButtonCount}
+      </button>
+  }
+}
+
+
+interface AppProps{
+  initialCount:number;
+}
+
+export function App(props:AppProps): JSX.Element {
+  const [count, setCount]:[number, React.Dispatch<React.SetStateAction<number>>] = React.useState(props.initialCount);
+
+  React.useEffect(()=>{
+
+
+
+
+    return ()=>{
+
+    }
+
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          JT's Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <button onClick={()=>{
+        // alert('something') 
+        setCount(count+1);
+      }} >Click Me {count}</button>
+      
+      <SecondButton otherButtonCount={count} />
+    
     </div>
   );
 }
 
-export default App;
+// export App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App initialCount={5}></App>
   </React.StrictMode>,
   document.getElementById('root')
 );
